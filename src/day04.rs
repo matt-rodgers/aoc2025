5d97b6d71c0e29e8220f5dbcdad8fb84b57f6526
Matt Rodgers <mattrodgers31@gmail.com>
2025-12-04 08:27:31 +0000

    day 4 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -14,9 +14,24 @@ impl Aoc for Day04 {
 }
 
 fn run_on_input(input: &str) -> (usize, usize) {
-    let grid: Grid = input.parse().unwrap();
-    let pt1 = grid.pt1();
-    (pt1, 0)
+    let mut grid: Grid = input.parse().unwrap();
+    let pt1 = grid.removable_rolls().count();
+
+    let mut pt2 = 0;
+
+    loop {
+        let removable: Vec<_> = grid.removable_rolls().collect();
+        if removable.is_empty() {
+            break;
+        }
+
+        for (x, y) in removable {
+            grid.remove(x, y);
+            pt2 += 1;
+        }
+    }
+
+    (pt1, pt2)
 }
 
 #[derive(Debug)]
@@ -25,28 +40,43 @@ struct Grid {
 }
 
 impl Grid {
-    fn pt1(&self) -> usize {
-        self.points
-            .iter()
-            .enumerate()
-            .flat_map(|(y, line)| {
-                line.iter().enumerate().filter(move |(x, point)| {
-                    // Only count points that are rolls of paper
-                    if **point != b'@' {
-                        return false;
-                    }
-
-                    neighboring_points_iter(*x, y)
-                        .filter(|(xn, yn)| self.get(*xn, *yn).map(|p| p == b'@').unwrap_or(false))
-                        .count()
-                        < 4
-                })
-            })
-            .count()
-    }
-
     fn get(&self, x: usize, y: usize) -> Option<u8> {
         self.points.get(y).and_then(|line| line.get(x)).copied()
+    }
+
+    fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut u8> {
+        self.points.get_mut(y).and_then(|line| line.get_mut(x))
+    }
+
+    fn remove(&mut self, x: usize, y: usize) {
+        if let Some(point) = self.get_mut(x, y) {
+            // Replace with 'x' as in example so that we can visualise which points are removed
+            *point = b'x';
+        }
+    }
+
+    fn is_removable(&self, x: usize, y: usize) -> bool {
+        neighboring_points_iter(x, y)
+            .filter(|(xn, yn)| self.get(*xn, *yn).map(|p| p == b'@').unwrap_or(false))
+            .count()
+            < 4
+    }
+
+    fn removable_rolls(&self) -> impl Iterator<Item = (usize, usize)> {
+        self.points.iter().enumerate().flat_map(move |(y, line)| {
+            line.iter().enumerate().filter_map(move |(x, point)| {
+                // Only count points that are rolls of paper
+                if *point != b'@' {
+                    return None;
+                }
+
+                if self.is_removable(x, y) {
+                    Some((x, y))
+                } else {
+                    None
+                }
+            })
+        })
     }
 }
 
@@ -90,7 +120,7 @@ mod tests {
     fn test_example() {
         let (pt1, pt2) = run_on_input(EXAMPLE_INPUT);
         assert_eq!(13, pt1);
-        assert_eq!(0, pt2);
+        assert_eq!(43, pt2);
     }
 
     #[test]
