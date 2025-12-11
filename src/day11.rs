6157311bf24da007e7ead7c7bfe936eb2a124027
Matt Rodgers <mrodgers@lunarenergy.com>
2025-12-11 08:33:11 +0000

    day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -13,7 +13,7 @@ impl Aoc for Day11 {
     }
 }
 
-fn run_on_input(input: &str) -> (u64, usize) {
+fn run_on_input(input: &str) -> (u64, u64) {
     let devices: HashMap<&str, Vec<&str>> = input
         .trim()
         .lines()
@@ -26,22 +26,51 @@ fn run_on_input(input: &str) -> (u64, usize) {
 
     let pt1 = count_paths("you", "out", &devices);
 
-    (pt1, 0)
+    // If we assume there are no cycles, we can only go from either fft -> dac OR dac --> fft.
+    // Figure out which one, and then go from svr --> xxx --> xxx --> out.
+    let dac_to_fft_paths = count_paths("dac", "fft", &devices);
+    let fft_to_dac_paths = count_paths("fft", "dac", &devices);
+    let pt2 = if dac_to_fft_paths == 0 {
+        count_paths("svr", "fft", &devices) * fft_to_dac_paths * count_paths("dac", "out", &devices)
+    } else if fft_to_dac_paths == 0 {
+        count_paths("svr", "dac", &devices) * dac_to_fft_paths * count_paths("fft", "out", &devices)
+    } else {
+        panic!("Either dac --> fft or fft --> dac must be zero to avoid cycles")
+    };
+
+    (pt1, pt2)
 }
 
 fn count_paths(from: &str, to: &str, edges: &HashMap<&str, Vec<&str>>) -> u64 {
-    match edges.get(from) {
-        None => 0,
-        Some(outputs) => outputs
-            .iter()
-            .map(|output| {
-                if output == &to {
-                    1
-                } else {
-                    count_paths(output, to, edges)
-                }
-            })
-            .sum(),
+    let mut memo = HashMap::new();
+    count_paths_memo(from, to, edges, &mut memo)
+}
+
+fn count_paths_memo<'a>(
+    from: &'a str,
+    to: &'a str,
+    edges: &HashMap<&'a str, Vec<&'a str>>,
+    memo: &mut HashMap<&'a str, u64>,
+) -> u64 {
+    match memo.get(from) {
+        Some(n) => *n,
+        None => match edges.get(from) {
+            None => 0,
+            Some(outputs) => {
+                let n = outputs
+                    .iter()
+                    .map(|output| {
+                        if output == &to {
+                            1
+                        } else {
+                            count_paths_memo(output, to, edges, memo)
+                        }
+                    })
+                    .sum();
+                memo.insert(from, n);
+                n
+            }
+        },
     }
 }
 
@@ -50,11 +79,17 @@ mod tests {
     use super::*;
 
     const EXAMPLE_INPUT: &str = include_str!("../inputs/11.ex");
+    const EXAMPLE_INPUT_2: &str = include_str!("../inputs/11.2.ex");
 
     #[test]
     fn test_example() {
-        let (pt1, pt2) = run_on_input(EXAMPLE_INPUT);
+        let (pt1, _) = run_on_input(EXAMPLE_INPUT);
         assert_eq!(5, pt1);
-        assert_eq!(0, pt2);
+    }
+
+    #[test]
+    fn test_example_2() {
+        let (_, pt2) = run_on_input(EXAMPLE_INPUT_2);
+        assert_eq!(2, pt2);
     }
 }
